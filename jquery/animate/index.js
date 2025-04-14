d6d743b1d7e8d381ab1c3959a2557d7d1934bf83
Awishka Isuru <awishkaisuru0573@gmail.com>
2025-04-14 20:25:31 +0530

    timming effect try

--- jquery/animate/index.js
+++ jquery/animate/index.js
@@ -1,7 +1,11 @@
-$(".circle").click(function(){
+$(".circle").hover(function(){
 
     $(this).animate({
 
         width:"250px" 
-    });
+    },500, function(){
+
+        $(this).css("background-color","red")
+    }
+    );
 });
